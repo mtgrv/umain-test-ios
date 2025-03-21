30d06a7d2a1533b7801edf872ac60ccebfe6d4e7
Matteo Gervasi <matteo@dsharp.it>
2025-03-21 13:01:24 +0100

    Initial commit

---
+++ UmainMobileTest/UmainMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmainMobileTestApp.swift
+//  UmainMobileTest
+//
+//  Created by Matteo Gervasi on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UmainMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
